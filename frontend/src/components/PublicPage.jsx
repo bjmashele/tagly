9d5fa5235a7882fc35b5de2d0cd964d896ee6e15
bjmashele <bjmashele@gmail.com>
2020-09-12 02:07:17 -0500

    Added more styles to public page

--- frontend/src/components/PublicPage.jsx
+++ frontend/src/components/PublicPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Flex, Text, Box, Link } from "rebass";
-
+import { Flex, Text, Box, Heading, Image } from "rebass";
+import image from "../assets/tagly-icon.svg";
 const Header = () => {
   return (
     <div>
@@ -11,24 +11,58 @@ const Header = () => {
         height="7vh"
         alignItems="center"
       >
-        <Text p={2} paddingRight="22vw" fontWeight="bold">
-          Tagly
+        <Text
+          p={2}
+          paddingRight="22vw"
+          fontWeight="bold"
+          fontFamily="Rockwell Extra Bold"
+        >
+          TAGLY
         </Text>
 
-        <Text px={6} color="#41444b">
+        <Text px={6} color="#41444b" className="text">
           Features
         </Text>
-        <Text color="#41444b">Login</Text>
+        <Text color="#41444b" className="text">
+          Login
+        </Text>
       </Flex>
     </div>
+  );
+};
+
+const Main = () => {
+  return (
+    <Box
+      sx={{
+        paddingTop: "15vh",
+        height: "75vh",
+        display: "grid",
+        // alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Heading fontSize={[3, 4, 5]} color=" #f0a500">
+        COLLECT, ORGANIZE, AND SHARE WEB LINKS WITH EASE.
+      </Heading>
+      <Image
+        src={image}
+        ml={7}
+        mt={-4}
+        sx={{
+          width: ["100%", "50%"],
+          display: "grid",
+          justifyContent: "center",
+        }}
+      />
+    </Box>
   );
 };
 export default function PublicPage() {
   return (
     <div>
       <Header></Header>
-
-      <div className="welcome">Welcome</div>
+      <Main />
     </div>
   );
 }
