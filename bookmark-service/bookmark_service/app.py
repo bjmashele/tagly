44924761a38dcfffc6ab77927cd6c8162390a291
bjmashele <bjmashele@gmail.com>
2020-09-22 17:38:48 -0500

    Connected api_namespace to the app

--- bookmark-service/bookmark_service/app.py
+++ bookmark-service/bookmark_service/app.py
@@ -1,10 +1,21 @@
 
 from flask import Flask
+from flask_restplus import Api
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello Tagly"
+def create_app():
+    from bookmarks_service.api_namespace import api_namespace
 
-app.run(host='0.0.0.0', port=8000)
+    application = Flask(__name__)
+    api = Api(application, version='0.1', title='Bookmarks Backend API',
+              description='Version 1 of Bookmarks API')
+
+    from bookmarks_service.db import db, db_config
+    application.config['RESTPLUS_MASK_SWAGGER'] = False
+    application.config.update(db_config)
+    db.init_app(application)
+    application.db = db
+
+    api.add_namespace(api_namespace)
+
+    return application
